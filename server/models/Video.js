64f29884ef6ec90628f7ac6fd0013a7c57b36a4e
tayyabaE <tayyabaejaz13375@gmail.com>
2026-01-07 19:51:55 +0500

    schema commit

--- server/models/Video.js
+++ server/models/Video.js
@@ -10,30 +10,20 @@ const CommentSchema = new Schema({
     consumer: { type: Schema.Types.ObjectId, ref: 'user', required: true }
 }, { timestamps: true })
 
-const PostSchema = new Schema({
+const VideoSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     genre: { type: String, required: true },
-
-    mediaType: { 
-        type: String, 
-        enum: ['video', 'image'], 
-        required: true 
-    },
-
-    url: { type: String, required: true }, // video OR image URL
-
-    likes: [LikeSchema],
-    comments: [CommentSchema],
+    likes: [ LikeSchema ],
+    comments: [ CommentSchema ],
     views: { type: Number, default: 0 },
-
     creator: { type: Schema.Types.ObjectId, ref: 'user', required: true },
+    url: { type: String, required: true },
     status: { type: Number, default: 1 }
-
 }, { timestamps: true })
 
-module.exports = mongoose.model('post', PostSchema)
-
+const Video = mongoose.model('video', VideoSchema)
+module.exports = Video
 
 // Status
 // 1 - Public
